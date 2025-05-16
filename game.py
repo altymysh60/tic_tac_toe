e5f09373ac21cc4bab7c6ee108be7325cc7fb10f
Altymysh WORK <abdyldaev.alt@gmail.com>
2025-05-16 17:37:59 +0300

    игра крестики/нолики

--- game.py
+++ game.py
@@ -1,17 +1,46 @@
 from gameparts import Board
-from gameparts.exceptions import FieldIndexError 
+from gameparts.exceptions import CellOccupiedError, FieldIndexError 
 
 def main():
     game = Board()
+    current_player = 'X'
+    running = True
     game.display()
-    row = int(input('Введите номер строки: '))
-    if row < 0 or row >= game.field_size:
-        raise FieldIndexError('Введено значение за границами игрового поля!')
-    column = int(input('Введите номер столбца: '))
-    
-    game.make_move(row, column, 'X')
-    print('Ход сделан!')
-    game.display()
+
+    while running:
+        
+        print(f'Ход делает {current_player}')
+
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+  
+        game.make_move(row, column, current_player)
+        game.display()
+        current_player = 'O' if current_player == 'X' else 'X'
 
 if __name__ == '__main__':
     main()
